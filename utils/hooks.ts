8248146f836d895627d2b6ce9607273e9b1e0bcf
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-02-11 11:06:49 +0530

    Framework

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -3,16 +3,17 @@ import { Page, test } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
 
     page.on('response', async () => {
-        const cookieButton = page.locator('button:has-text("Accepter")');
-        if (await cookieButton.isVisible()) {
-            await cookieButton.click();
+        try {
+            const cookieButton = page.locator('button:has-text("Accepter")');
+            if (await cookieButton.isVisible()) {
+                await cookieButton.click();
+            }
+        } catch (error) {
+            
         }
     });
-    });
-
-
+});
 test.afterEach(async ({ page }) => {
     await page.close()
   
 });
-
